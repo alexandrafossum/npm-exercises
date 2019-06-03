fd06e5c51dbb510d9cc90540edae92a938fbd8d1
Alexandra <fossum.alexandra@gmail.com>
2019-06-03 13:45:09 -0500

    post walkthrough

--- IdeaProjects/database-exercises/albums_migration.sql
+++ IdeaProjects/database-exercises/albums_migration.sql
@@ -2,10 +2,11 @@ USE codeup_test_db;
 DROP TABLE IF EXISTS albums;
 CREATE TABLE albums (
   id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  artist varchar(255),
-  name varchar(255),
-  release_date YEAR,
-  sales decimal(9),
-  genre varchar(50)
+  artist varchar(255) NOT NULL,
+  name varchar(255) NOT NULL,
+  release_date INT NOT NULL,
+  sales decimal(10, 2),
+  genre varchar(50),
+  primary key (id)
 );
 
